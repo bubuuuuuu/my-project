6f3a51510fbce72058d12e67494697b8366c74ca
wlq <2437960421@qq.com>
2023-09-20 15:29:51 +0800

    个人页面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  头像 = "无"
+  昵称 = "wangliqiong"
+  学号 = 2310273022
 </template>
 
 <script>
